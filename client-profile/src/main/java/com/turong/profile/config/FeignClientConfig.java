a12aa9c22a5b9993d6eb1f5a9c57ea818f8cb878
turong <truong.le@binance.com>
2021-07-26 01:45:03 +0700

    Forwarder the Feign request header to another service - use interceptor pattern to add Feign request headers

--- client-profile/src/main/java/com/turong/profile/config/FeignClientConfig.java
+++ client-profile/src/main/java/com/turong/profile/config/FeignClientConfig.java
@@ -1,6 +1,9 @@
 package com.turong.profile.config;
 
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -9,4 +12,15 @@ import org.springframework.core.annotation.Order;
 @Configuration
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class FeignClientConfig {
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate requestTemplate) {
+                requestTemplate.header(TenantHeaderFilter.X_TENANT_ID, AppContextHolder.getTenant());
+            }
+        };
+    }
+
 }
