7e6dd9e057aaf92412537b0c4bfe4284dae1cd43
turong <truong.le@binance.com>
2021-07-29 20:12:51 +0700

    - Add Feign configuration

--- client-profile/src/main/java/com/turong/profile/feign/account/AccountApi.java
+++ client-profile/src/main/java/com/turong/profile/feign/account/AccountApi.java
@@ -1,6 +1,5 @@
 package com.turong.profile.feign.account;
 
-import com.turong.profile.config.FeignClientConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
         name = "client-account",
-        url = "http://host.docker.internal:8580/client-account",
-        configuration = {FeignClientConfig.class}
+        url = "http://host.docker.internal:8580/client-account"
 )
 // if we do not use ribbon for load balancer, then we use url instead of name.
 // otherwise, we use instanceId as name of the service
